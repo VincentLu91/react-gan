97cd58dd5e4a20877dc975c5beee4973d3000d4a
Vince Lu <vincentlu@hotmail.ca>
2022-03-07 16:34:37 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import contentTypes from "./api/contentTypes";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Select from "react-select";
 
 function App() {
+  const [ganBase64, setGanBase64] = useState(null);
+  const [showImage, setShowImage] = useState(false);
+  const [contentLabel, setContentLabel] = useState(null);
+  const [contentAPI, setContentAPI] = useState(null);
+
+  const getImage = async () => {
+    if (!contentAPI) {
+      return;
+    }
+    try {
+      const resp = await axios.post(
+        `${contentAPI.api_address}${contentAPI.route_path}`
+      );
+      console.log(resp.data);
+      return resp.data;
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.status);
+        console.log(error.response);
+      } else {
+        console.log(error);
+      }
+    }
+  };
+
+  // handle onChange event of the dropdown
+  const handleChange = (e) => {
+    setContentLabel(e.label);
+    console.log("Image Type selected: ", e.label);
+    setContentAPI(e.value);
+    console.log("Image API selected: ", e.value);
+  };
+
+  const displayContent = showImage && (
+    <img alt="uploaded" src={`data:image/png;base64,${ganBase64}`} />
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Select
+        placeholder="Select Option"
+        value={contentTypes.find((obj) => obj.value === contentLabel)} // set selected value
+        options={contentTypes} // set list of the data
+        onChange={handleChange} // assign onChange function
+      />
+      <button
+        onClick={async () => {
+          const result = await getImage();
+          if (result) {
+            const ganImage = Object.entries(result)[0];
+            alert(ganImage[1]);
+            setGanBase64(ganImage[1]);
+            setShowImage(true);
+          } else {
+            alert("nothing generated");
+          }
+        }}
+      >
+        Get Image
+      </button>
+      {/*displayContent*/}
+      {contentLabel && displayContent}
     </div>
   );
 }
---
+++ src/api/contentTypes.js
@@ -0,0 +1,18 @@
+const contentTypes = [
+  {
+    value: {
+      api_address: "ENDPOINT_URL",
+      route_path: "/generate",
+    },
+    label: "Face1",
+  },
+  {
+    value: {
+      api_address: "ENDPOINT_URL",
+      route_path: "/generate",
+    },
+    label: "Face2",
+  },
+];
+
+export default contentTypes;
